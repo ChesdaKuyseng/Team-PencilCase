a4e3e951969452ffb1dee96daba8cf20f486af4b
Chesda <156415782+ChesdaKuyseng@users.noreply.github.com>
2024-04-07 19:07:43 +0700

    Update exercise-2.py

--- exercise-2.py
+++ exercise-2.py
@@ -1,2 +1,11 @@
-def index_power(numnbers, n):
-    ...
+def index_power(array, n):
+    if n < len(array):
+        return array[n] ** n
+    else:
+        return -1
+def main():
+    n = int(input('Input n = '))
+    array = [1, 0]
+    print(index_power(array,n))
+if __name__ == "__main__":
+    main()
