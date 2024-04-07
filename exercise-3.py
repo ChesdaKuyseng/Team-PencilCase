513b86559ab0fb265fe3f215e744e53ba2ff8610
2024067veasna@aupp.edu.kh <156407452+DaryVeasna@users.noreply.github.com>
2024-04-07 20:42:46 +0700

    Finish exercise-3.py

--- exercise-3.py
+++ exercise-3.py
@@ -1,2 +1,24 @@
 def remove_all_after(numbers, n):
-    ...
+    if numbers == []:
+        return numbers  
+    
+    try:
+        index_to_remove = numbers.index(n)  
+        result = numbers[:index_to_remove + 1]  
+        return result
+    except ValueError:
+        return numbers 
+
+def main():
+    numbers_input = input("Please enter your numbers in list by commas: ")
+    n_input = int(input("Please enter the number to remove all after: "))
+    
+    numbers_str = numbers_input.replace('[', '').replace(']', '')
+    numbers = [int(num.strip()) for num in numbers_str.split(',')]
+    
+    result = remove_all_after(numbers, n_input)
+    print("Result:", result)
+
+if __name__ == "__main__":
+    main()
+
