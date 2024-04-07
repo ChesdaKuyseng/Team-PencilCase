a7c1f0c1d8dc59d9524231ccfc7c22604d7f0d02
2024067veasna@aupp.edu.kh <156407452+DaryVeasna@users.noreply.github.com>
2024-04-07 20:43:45 +0700

    Finish exercise-4.py

--- exercise-4.py
+++ exercise-4.py
@@ -1,2 +1,25 @@
 def chunking_by(numbers, chunck):
-    ...
+    if numbers:
+        chunk_size = []
+        for i in range(0, len(numbers), chunk):
+            chunk_size.append(numbers[i:i + chunk])
+        return chunk_size
+    else:
+        return []
+
+def main():
+    
+    numbers_input = input("Enter numbers separated by commas: ")
+    chunk_size_input = int(input("Enter chunk size: "))
+    
+    numbers_str = numbers_input.replace('[', '').replace(']', '')
+    numbers = [int(num.strip()) for num in numbers_str.split(',')]
+    
+    chunks = chunking_by(numbers, chunk_size_input)
+    print(chunks)
+
+if __name__ == "__main__":
+    main()
+
+
+
