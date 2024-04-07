3aa2db5ce24ca3fb6f8df66748ab1bbce91a6029
Chesda <156415782+ChesdaKuyseng@users.noreply.github.com>
2024-04-07 18:50:39 +0700

    Update exercise-1.py

--- exercise-1.py
+++ exercise-1.py
@@ -1,2 +1,15 @@
-def replace_last(numbers):
-    ...
+list = [1,2,3,4,]
+def replace_last(list):
+    new_list = list
+    if len(new_list) < 1:
+        return new_list
+    else:
+        first = new_list[-1]
+        new_list.insert(0,first)
+        del new_list[-1]
+        return new_list        
+def main():
+    print(f'{list} --> {replace_last(list)}')
+
+if __name__ == "__main__":
+    main()
